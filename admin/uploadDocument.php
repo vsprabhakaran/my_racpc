5dff8ca2a9af8c356fb5d627dc9ce5e6d1652e3a
vsprabhakaran <v.s.prabhakaran@gmail.com>
2014-11-10 16:43:23 +0530

    Jagan-barcodeViewOnUpload

--- admin/uploadDocument.php
+++ admin/uploadDocument.php
@@ -12,6 +12,7 @@
         }
     ?>
     <script type="text/javascript" src="../jquery-latest.min.js"></script>
+	<script type="text/javascript" src="../jquery-barcode.js"></script>
     <script type="text/javascript">
 
         function accountNumButtonClick() {
@@ -33,12 +34,14 @@
 
         }
         function validAccountNumberEnterred() {
+			var acc= $("#accNumber").val();
             document.getElementById('getAccountDetailsSpan').style.visibility = "visible";
             document.getElementById('accNumber').style.backgroundColor = "#CCFFCC";
             $('#file').prop('disabled', false);
             $('#folio_no').prop('disabled', false);
             $('#rack_no').prop('disabled', false);
             $('#formButton').prop('disabled', false);
+			$("#bcTarget").barcode(acc, "code128",{barWidth:2, barHeight:30});
         }
         function invalidAccountNumberEnterred() {
             document.getElementById('accNumber').style.backgroundColor = "#FFC1C1";
@@ -86,12 +89,14 @@
     </script>
 	<br/><br/>
 <div>
+
    <form id="formid" class="pure-form pure-form-aligned" action="uploadDocumentAction.php" method="post" enctype="multipart/form-data">
    <div class="pure-control-group">
             <label for="accNumber" >Account Number</label>
             <input type="text" id="accNumber" name="accNumber" autocomplete="off" onkeydown="if (event.keyCode == 13) accountNumButtonClick()" />
             <!--<span><button name="accButton" onclick="accountNumButtonClick()">Go</button></span> -->
             <a id="getAccountDetailsSpan" href="#"  style="visibility: hidden" onclick="showAccountDetails()">View Details</a> 
+			<div id="bcTarget" ></div>
     </div>
     <div class="pure-control-group">
 		<label for="file">Choose file</label>
